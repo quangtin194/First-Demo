6aa5c81ad48aacd2e956a49c52280e2f1892cfd8
QuangTin <tinforwas@gmail.com>
2026-06-22 00:15:41 +0700

    Update output message for sum calculation

--- test.c
+++ test.c
@@ -6,6 +6,6 @@ int main() {
     printf("Nhap a: "); scanf("%d", &a);
     printf("Nhap b: "); scanf("%d", &b);
     int sum = a + b;
-    printf("Tong: %d", sum);
+    printf("Tong 2 so la: %d", sum);
     return 0;
 }
