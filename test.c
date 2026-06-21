cd9a15e0a4679fe96c246ba8dcbb4c9169ed2a72
QuangTin <tinforwas@gmail.com>
2026-06-22 00:01:18 +0700

    remove "la"

--- test.c
+++ test.c
@@ -6,6 +6,6 @@ int main() {
     printf("Nhap a: "); scanf("%d", &a);
     printf("Nhap b: "); scanf("%d", &b);
     int sum = a + b;
-    printf("Tong la: %d", sum);
+    printf("Tong: %d", sum);
     return 0;
 }
